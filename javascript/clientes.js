61fb7e807eb116f0fea784b739767fa89575f96a
carlossantanaf <contatocalsf@hotmail.com>
2022-06-09 01:12:13 -0300

    repsonsivoclientes

--- javascript/clientes.js
+++ javascript/clientes.js
@@ -2,12 +2,13 @@ export default function clientes() {
 
   const quant = document.querySelectorAll('.clientes-carro img')
   let atual = 0
+  let altura = -616;
   const bola = document.querySelectorAll('.bola')
   let rolagem = true
   bola[0].classList.add('ativo')
   bola.forEach((item, index) => {
     item.addEventListener('click', () => {
-      quant[0].style.marginTop = -616*index+'px'
+      quant[0].style.marginTop = altura*index+'px'
       bola.forEach((item) => {
         item.classList.remove('ativo')
       })
@@ -28,12 +29,21 @@ export default function clientes() {
       if(atual >= quant.length) {
         atual=0
       } 
-      quant[0].style.marginTop = -616*atual+'px'
+      quant[0].style.marginTop = altura*atual+'px'
       bola[atual].classList.add('ativo')
     } else {
       rolagem = true
     }
     }, 4000)
+
+    const small = window.matchMedia('(max-width: 700px)');
+    if(small.matches) {
+      altura = -436;
+    }
+    const smallDois = window.matchMedia('(max-width: 550px)');
+    if(smallDois.matches) {
+      altura = -316;
+    }
 
   
 }
