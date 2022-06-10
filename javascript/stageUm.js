4a82aa9a416fc159529977b3178c84fe12c3f3f6
carlossantanaf <contatocalsf@hotmail.com>
2022-06-10 20:57:28 -0300

    footer

--- javascript/stageUm.js
+++ javascript/stageUm.js
@@ -2,7 +2,7 @@ export default function stageUm() {
   const open = document.querySelector('.open')
   const close = document.querySelector('.close')
   const menu = document.querySelector('.menuR')
-
+  
   function openMenu () {
     menu.classList.add('ativo')
     open.classList.add('none')
@@ -16,7 +16,7 @@ export default function stageUm() {
   }
 
   function closeTarget({target}) {
-    if((target.classList.contains('menuR') || target.classList.contains('close') || target.classList.contains('open') || target.classList.contains('header') || target.classList.contains('logo')) === false && close.classList.contains('initial')) {
+    if((target.classList.contains('menuR') || target.classList.contains('close') || target.classList.contains('open') || target.classList.contains('header') || target.classList.contains('bola') ||target.classList.contains('logo')) === false && close.classList.contains('initial')) {
       closeMenu()
     }
  
